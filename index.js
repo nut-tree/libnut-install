bc86e1216ddbada029b5e197811b0ea67d8efb94
Simon Hofmann <mail@simon-hofmann.org>
2022-01-04 16:28:13 +0100

    Change imports so they are picked up when packaging an Electron app

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-const libnut= require(`@nut-tree/libnut-${process.platform}`);
+const libnut = (() => {
+	switch (process.platform) {
+		case 'win32':
+			return require(`@nut-tree/libnut-win32`);
+		case 'linux':
+			return require(`@nut-tree/libnut-linux`);
+		case 'darwin':
+			return require(`@nut-tree/libnut-darwin`);
+	}
+})();
 
 module.exports = libnut;
