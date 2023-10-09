d60fc5958b960bf0d5e3234b737cf0856223766a
Simon Hofmann <mail@simon-hofmann.org>
2023-10-09 21:05:30 +0200

    Update typings to add move/resize/focus functions for windows

--- index.d.ts
+++ index.d.ts
@@ -32,9 +32,9 @@ export interface Rect {
 export function setKeyboardDelay(ms: number): void;
 export function keyTap(key: string, modifier?: string | string[]): void;
 export function keyToggle(
-  key: string,
-  down: string,
-  modifier?: string | string[]
+    key: string,
+    down: string,
+    modifier?: string | string[]
 ): void;
 export function typeString(string: string): void;
 export function typeStringDelayed(string: string, cpm: number): void;
@@ -51,5 +51,32 @@ export function getWindows(): number[];
 export function getActiveWindow(): number;
 export function getWindowRect(handle: number): Rect;
 export function getWindowTitle(handle: number): string;
+
+/**
+ * Sets the focus to a specific window using its handle.
+ *
+ * @param {number} handle - The handle ID of the window to be focused.
+ * @returns {void}
+ */
+export function focusWindow(handle: number): void
+
+/**
+ * Resizes a window by its handle to the given width and height.
+ * The window is moved to the x & y coordinates if specified.
+ *
+ * @param {number} handle - The handle ID of the window to be resized.
+ * @param {Size} newSize - The new size of the window.
+ * @returns {void}
+ */
+export function resizeWindow(handle: number, newSize: Size): void
+
+/**
+ * Moves a window by its handle to the given x and y coordinates.
+ *
+ * @param {number} handle - The handle ID of the window to be resized.
+ * @param {Point} newOrigin - The new size of the window.
+ * @returns {void}
+ */
+export function moveWindow(handle: number, newOrigin: Point): void
 
 export const screen: Screen;
